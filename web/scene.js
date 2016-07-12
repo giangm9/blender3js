87fec0ccde3764e74f210ee264ae7ea81aa9bb0c
giang.le <giang.le@nccsoft.vn>
2016-07-13 00:10:49 +0700

    update js scene

--- web/scene.js
+++ web/scene.js
@@ -1,3 +1,52 @@
-/// <reference path="three/three.js" />
+var SCREEN_WIDTH = window.innerWidth;
+var SCREEN_HEIGHT = window.innerHeight;
 
 var container;
+var camera, scene;
+var renderer;
+
+init();
+animate();
+
+function init() {
+    container = document.getElementById('container');
+    camera = new THREE.PerspectiveCamera(30, SCREEN_WIDTH / SCREEN_HEIGHT, 1, 10000);
+    camera.position.z = 10;
+    scene = new THREE.Scene();
+
+
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+
+    renderer.setClearColor(0xffffff);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    renderer.domElement.style.position = "relative";
+    container.appendChild(renderer.domElement);
+
+    var loader = new THREE.ObjectLoader();
+    loader.load("res/simple.json", function(loadedScene) {
+        scene = loadedScene;
+        scene.add(camera);
+        scene.fog = new THREE.Fog(0xffffff, 2000, 10000);
+        
+        cube = scene.children[1];
+        cube.material.skinning = true;
+        bones = cube.skeleton.bones;
+
+    });
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    render();
+}
+
+function render() {
+
+    camera.position.x = 5;
+    camera.position.y = 6;
+    camera.lookAt(scene.position);
+    renderer.render(scene, camera);
+}
