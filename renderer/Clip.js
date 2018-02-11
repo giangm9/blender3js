47817c4409ceefb98d2260142c4ea60c3c51b9b4
giangm9 <giangm9@gmail.com>
2018-02-11 19:00:53 +0700

    add stats

--- renderer/Clip.js
+++ renderer/Clip.js
@@ -3,24 +3,27 @@
  */
 
 import * as THREE from './libs/three.js';
-import load from "./BlendLoader.js"
-import "./OrbitControls.js"
+import load from "./BlendLoader.js";
+import "./OrbitControls.js";
+import Stats from "./stats.js";
 
 const CONTROL_MIN_DISTANCE = 10;
 const CONTROL_MAX_DISTANCE = 50;
 
-function Clip(){}
+function Clip() { }
 
-Clip.prototype.size = function(){
+Clip.prototype.size = function () {
   return {
     width: this.container.clientWidth,
     height: this.container.clientHeight
   }
 }
 
-Clip.prototype.render = function(url, container, callback){
+Clip.prototype.render = function (url, container, callback) {
   this.container = container;
-  this.renderer = new THREE.WebGLRenderer();
+  this.renderer = new THREE.WebGLRenderer({ antialias: true });
+  this.stats = new Stats();
+  container.appendChild(this.stats.dom);
 
   // fix to disable "extension 'GL_ARB_gpu_shader5' is not supported" log
   // ref : https://github.com/mrdoob/three.js/issues/9716
@@ -29,7 +32,7 @@ Clip.prototype.render = function(url, container, callback){
   var width = this.size().width;
   var height = this.size().height;
   var that = this;
-  this.camera = new THREE.PerspectiveCamera(75, width/height, 0.1, 1000);
+  this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
   var controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
 
   container.appendChild(this.renderer.domElement);
@@ -38,21 +41,22 @@ Clip.prototype.render = function(url, container, callback){
   controls.minDistance = CONTROL_MIN_DISTANCE;
   controls.maxDistance = CONTROL_MAX_DISTANCE;
 
-  load(url, (scene) => {
+  load(url, ( scene ) => {
     that.scene = scene;
     controls.reset();
     that.animate();
-    if (callback){
+    if (callback) {
       callback(this);
     }
   });
 };
 
-Clip.prototype.animate = function(){
+Clip.prototype.animate = function () {
   var size = this.size();
   requestAnimationFrame(this.animate.bind(this));
   this.renderer.setSize(size.width, size.height);
   this.renderer.render(this.scene, this.camera);
+  this.stats.update();
 };
 
 export default Clip
